3a151821c0561a69d148e288e718a891cc45ec3b
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-07 04:58:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  const two = 2
+    const two = 2
   return n += 2
 
   // Feel free to move things around!
 
 }
-function funky(){}
-var funkyFunction = function funky() {
+
+var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
@@ -30,4 +30,7 @@ var funkyFunction = function funky() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var funky = funkyFunction
+var theFunk = funkyFunction
+function theFunk(){
+  return funkyFunction
+}
