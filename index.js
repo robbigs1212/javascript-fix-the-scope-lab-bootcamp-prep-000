5bac58c226e1fee4df804bda4d49be8fae5128e8
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-07 04:47:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  
+    const two = 2
   return n += 2
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
